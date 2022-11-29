885658ca3bf9f208437105e21b5947095c44ff44
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-11-29 15:23:00 +0100

    error handling middleware

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 require("./src/db/mongoose")
 const router = require("./src/routers/post")
+const errorHandler = require("./src/middleware/errorHandler")
 const app = express()
 
 
@@ -10,15 +11,15 @@ app.set("views", "./src/views")
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}))
 app.use(router)
+app.use(errorHandler)
 
 app.get("/", (req, res) => {
   res.render("home")
-});
+})
 
 app.get("/about", async (req, res) => {
   res.render("home")
 })
-
 
 app.get("/home", async (req, res) => {
   try {
@@ -27,6 +28,5 @@ app.get("/home", async (req, res) => {
     res.status(400).send(err);
   }
 });
-
 
 app.listen(3000)
