8c7fb740a482f371ebd16665a752d6c0991c7e10
Andrea Spiga <drespriz@gmail.com>
2022-11-29 18:47:32 +0100

    connects react POST request with express, creates new Post with images saved on cloudinary thorugh react interface

--- src/routes/post.js
+++ src/routes/post.js
@@ -30,39 +30,7 @@ postRouter.get('/rawPosts', async (req, res) => {
   }
 });
 
-postRouter.post('/posts', multerUpload, (req, res) => {
-  console.log(req.body)
-  console.log(req.files)
-})
-
-// postRouter.post('/posts', (req, res) => {
-//   multerUpload(req, res, async (err) => {
-//     if (err instanceof multer.MulterError) {
-//       res.send(err.message);
-//     } else if (err) {
-//       res.status(400).send(err);
-//     } else {
-//       const post = new Post(req.body);
-//       const savedPost = await post.save();
-//       const images = req.files;
-//       await Promise.all(images.map(async (image) => {
-//         const data = await uploadToCloudinary(image.path, 'emaJons_dev');
-//         const newImage = new Image({
-//           publicId: data.public_id,
-//           imageUrl: data.url,
-//         });
-//         await Post.updateOne(
-//           { _id: savedPost._id },
-//           { $addToSet: { images: [newImage]}}
-//         );
-//       }))
-//       res.redirect('/posts');
-//     }
-//   })
-// })
-
-// postRouter.delete('/image/:id', async (req, res) => {
-postRouter.post('/posts', upload.array('postImages', 20), async (req, res) => {
+postRouter.post('/posts', multerUpload, async (req, res) => {
   try {
     const post = new Post(req.body);
     const savedPost = await post.save();
