36e75cca1a9e694dba06248ab78d452a4b3e51ca
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-12-01 18:21:13 +0100

    create routes for single posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Category from './components/category/Category';
+import PostForm from './components/postForm/PostForm';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Link
 } from 'react-router-dom';
+import axios from 'axios';
 
 export default function App() {
 
@@ -15,10 +17,12 @@ export default function App() {
         <ul>
           <li><Link to="/walls">Walls</Link></li>
           <li><Link to="/sketchbooks">Sketchbooks</Link></li>
+          <li><Link to="/admin/dashboard">Dashboard</Link></li>
         </ul>
         <Routes>
           <Route exact path='/walls' element={<Category categoryName="walls" />}></Route>
           <Route exact path='/sketchbooks' element={<Category categoryName="sketchbooks" />}></Route>
+          <Route exact path='/admin/dashboard' element={<PostForm />}></Route>
         </Routes>
       </div>
     </Router>
--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import GridPost from '../post/GridPost';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 
 export default function Category({ categoryName }) {
   const [posts, setPosts] = useState([])
@@ -14,13 +19,14 @@ export default function Category({ categoryName }) {
   }, [])
 
   const gridPostElements = posts.map((post) => {
-    return (
-      <GridPost
-        image={post.images[0]}
-        id={post._id}
-        key={post._id}
-      />
-    )
+    return (post.images.length) ?
+      <Link to={`${post._id}`} id={post._id} key={post._id} >
+        <img src={post.images[0].imageUrl}/>
+      </Link> : "";
+  })
+
+  const postRoutesElements = posts.map((post) => {
+    <Route exact path={`${post._id}`} element={<Post />}></Route>
   })
 
   return (
@@ -28,14 +34,18 @@ export default function Category({ categoryName }) {
       <div className="category-container">
         <div className="tags-container">
           <ul>
-            <li>2021</li>
-            <li>2020</li>
+            <Link to="/"><li>2021</li></Link>
+            <Link to="/"><li>2020</li></Link>
           </ul>
         </div>
+
         <div className="posts-grid">
           {gridPostElements}
         </div>
       </div>
+      <Routes>
+        {postRoutesElements}
+      </Routes>
     </div>
   )
 }
