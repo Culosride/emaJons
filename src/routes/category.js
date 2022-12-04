ef41b6784512a0772d611ad28d0c6d1fb597dee3
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-12-04 16:25:06 +0100

    removes log statement in router

--- src/routes/category.js
+++ src/routes/category.js
@@ -4,7 +4,6 @@ const categoryRouter = new express.Router();
 const _ = require('lodash');
 
 categoryRouter.get('/api/categories/:category', async (req, res) => {
-  console.log(_.capitalize(req.params.category))
   try {
     const category = await Category.find({name: _.capitalize(req.params.category)});
     res.json(category);
