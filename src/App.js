9538cec64722828e6b19755919ee4507d2e953c5
Natalie Rauh <97293748+Nat-Ma@users.noreply.github.com>
2022-12-13 17:43:13 +0100

    merged slider and tags branches

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from './components/header/Header';
 import AllPosts from './components/allPosts/AllPosts';
 import PostForm from './components/postForm/PostForm';
 import Post from './components/post/Post';
 import Bio from './components/bio/Bio';
 import Contact from './components/contact/Contact';
-import { useParams } from 'react-router-dom';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from 'react-router-dom';
+
 export default function App() {
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
-          <Route exact path='/posts/:category' element={<AllPosts />} />
-          <Route path="/posts/:category/:postId" element={<Post />} />
+          <Route exact path='/:category' element={<AllPosts />} />
+          <Route path="/:category/:postId" element={<Post />} />
           <Route exact path='/bio' element={<Bio />}></Route>
           <Route exact path='/contact' element={<Contact />}></Route>
           <Route exact path='/admin/dashboard' element={<PostForm />}></Route>
