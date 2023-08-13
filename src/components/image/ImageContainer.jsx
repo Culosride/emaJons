7e4b54434cc8728595827e09799be0a583a56974
Andrea Spiga <drespriz@gmail.com>
2023-08-13 19:44:16 +0200

    handles AllPosts image hovering with CSS only

--- src/components/image/ImageContainer.jsx
+++ src/components/image/ImageContainer.jsx
@@ -2,25 +2,10 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const ImageContainer = ({ src, alt, hoverContent, linkUrl }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
   return (
-    <Link
-      reloadDocument
-      to={linkUrl}
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <img className="" src={src} alt={alt} />
-      {isHovered && <div className="post-info">{hoverContent}</div>}
+    <Link reloadDocument to={linkUrl} className="image-container">
+      <img className="image" src={src} alt={alt} />
+      <div className="post-info">{hoverContent}</div>
     </Link>
   );
 };
