d6295ce57b4b6f114c20ad0918757746aeb97b10
Andrea Spiga <drespriz@gmail.com>
2024-01-20 00:57:29 +0100

    fixes useScroll class handling

--- src/hooks/useScroll.jsx
+++ src/hooks/useScroll.jsx
@@ -1,15 +1,20 @@
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 export const useScroll = (customRef, cb = () => {}, options = {}) => {
   const { threshold, scrollClass } = options;
+  const savedScrollY = useSelector(state => state.ui.scrollPosition)
 
   useEffect(() => {
-    let lastScrollY = window.scrollY;
+    let lastScrollY = savedScrollY;
 
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
       const currentRef = customRef.current
-      if (currentScrollY <= threshold) return currentRef?.classList.remove(scrollClass);
+
+      if (currentScrollY <= threshold) {
+        return currentRef?.classList.remove(scrollClass)
+      };
 
       if (currentScrollY > lastScrollY) {
         currentRef?.classList.add(scrollClass);
